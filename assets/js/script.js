5cf4108bea6b1d6efa2601a7f84fc8fa5b677cae
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2022-11-10 20:51:03 +0100

    testimonial finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,26 @@ request.send();
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper(".testimonials__container",{
+    spaceBetween: 24,
+    loop: true,
+    grabCursor : true,
+    pagination : {
+        el:".swiper-pagination",
+        clickable : true,
+    },
+
+    breakpoints:{
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        }
+
+    }
+})
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
