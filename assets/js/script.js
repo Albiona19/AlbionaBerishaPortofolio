ecc1027cf65275ffe0fa9a3561f29d79d3aaf923
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2022-11-12 17:24:39 +0100

    media query

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,9 @@ request.onload = function () {
     statusHTML += '</tr>';
   });
   $('tbody').html(statusHTML);
-}
+
+  }
+
 
 // Send request
 request.send();
@@ -85,30 +87,32 @@ let swiperTestimonial = new Swiper(".testimonials__container",{
 
 
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop -58,
-              sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href *=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href *=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
-
 /*=============== LIGHT DARK THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'fa-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme)
+
+}
 
 
+
+themeButton.addEventListener('click', ()=> {
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
